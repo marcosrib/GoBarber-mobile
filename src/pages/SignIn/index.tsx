1c93e251e22787fd98996cbc4359c004b5e65ed4
marcos <marcos.rib.sousa@gmail.com>
2020-09-30 00:23:52 -0300

    add fonts

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { Image } from 'react-native';
 import logoImg from '../../assets/logo.png';
 
-import { Container } from './styles';
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => (
   <Container>
     <Image source={logoImg} />
+    <Title>Faça seu logo</Title>
   </Container>
 );
 
