250153c1dfa4ffb2bcc4887903cd8860942660e0
marcos <marcos.rib.sousa@gmail.com>
2020-09-30 00:04:57 -0300

    add logo

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Image } from 'react-native';
+import logoImg from '../../assets/logo.png';
 
 import { Container } from './styles';
 
-const SignIn: React.FC = () => <Container />;
+const SignIn: React.FC = () => (
+  <Container>
+    <Image source={logoImg} />
+  </Container>
+);
 
 export default SignIn;
