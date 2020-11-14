3d4109cfad3a35431c4ae754742544eea06959ec
marcos <marcos.rib.sousa@gmail.com>
2020-11-13 21:01:20 -0300

    created routes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -3,19 +3,28 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Dashboard from '../pages/Dashboard';
 
-const Auth = createStackNavigator();
+import AppointmentCreated from '../pages/AppointmentCreated';
+
+import CreateAppointment from '../pages/CreateAppointment';
+
+import Profile from '../pages/Profile';
+
+const App = createStackNavigator();
 
 const AppRoutes: React.FC = () => (
-  <Auth.Navigator
+  <App.Navigator
     screenOptions={{
       headerShown: true,
       cardStyle: { backgroundColor: '#312e28' },
     }}
 
   >
-    <Auth.Screen name="Dashboard" component={Dashboard} />
+    <App.Screen name="Dashboard" component={Dashboard} />
+    <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
+    <App.Screen name="CreateAppointment" component={CreateAppointment} />
 
-  </Auth.Navigator>
+    <App.Screen name="Profile" component={Profile} />
+  </App.Navigator>
 );
 
 export default AppRoutes;
